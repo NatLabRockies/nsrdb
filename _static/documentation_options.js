f56a495baa518de5caf3ab11c95f25e276225b17
bnb32 <bnb32@users.noreply.github.com>
2026-06-08 23:48:29 +0000

    Merge pull request #89 from NatLabRockies/bnb/gk2a
    Bespoke data model for GK2A data. NASA and GK2A data models now subclass base model. Eventually this should be refactored to rely more on nsrdb_vars.csv for variable definitions.

---
+++ _static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '4.1.4.dev18+gd53d20385',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
